28074cdd337f91f47fc2c678236b9af5120c3376
Paul Houser <paul@plaidscreen.com>
2016-01-12 08:37:08 -0500

    Minor fixes and CRLF removal

--- fields/media.php
+++ fields/media.php
@@ -4,29 +4,32 @@ namespace OUW\MetaBox;
 
 use \OUW as OUW;
 
-function media_template($template, $field, $post, $value) 
+function media_template($template, $field, $post, $value)
 {
-        
+
     wp_enqueue_media();
     wp_enqueue_script('metabox-media-selector', OUW\theme_url(__DIR__."/media.js"), array("jquery"), null, true);
-    
+
     if (!empty($value)) {
         $value = unserialize($value[0]);
     }
-    
+
     $attachments = "";
     $attachmentIds = "";
-    foreach($value as $attId) {
-        //$att = basename(get_attached_file($attId));
-        
-        $image = wp_get_attachment_image($attId, 'thumbnail', true);
-        $filename = basename(get_attached_file($attId));
-        
-        $attachments .= "<li data-id='{$attId}'><figure>$image<figcaption>$filename</figcaption></figure></li>";
-        $attachmentIds .= sprintf('{%s}', $attId);
+    if (!empty($value))
+    {
+        foreach($value as $attId)
+        {
+            //$att = basename(get_attached_file($attId));
+
+            $image = wp_get_attachment_image($attId, 'thumbnail', true);
+            $filename = basename(get_attached_file($attId));
+
+            $attachments .= "<li data-id='{$attId}'><figure>$image<figcaption>$filename</figcaption></figure></li>";
+            $attachmentIds .= sprintf('{%s}', $attId);
+        }
     }
-    
-    
+
     return '
         <div class="field">
             <input type="hidden" name="%2$s" class="media-selection" value="'.$attachmentIds.'">
@@ -35,30 +38,30 @@ function media_template($template, $field, $post, $value)
             '.$attachments.'
             </ul>
         </div>';
-    
+
 }
 \add_filter("MetaBox/render_field/media/template", __NAMESPACE__.'\media_template', 9, 4);
 
-function media_save_value($value) 
+function media_save_value($value)
 {
-    
+
     $values = explode("}{", trim($value, '{}'));
-    
+
     return $values;
-    
+
 }
 \add_filter("MetaBox/save_field/media", __NAMESPACE__."\media_save_value", 9, 1);
 
-function get_post_media($post_id, $key, $size="thumbnail", $attrs=array()) 
+function get_post_media($post_id, $key, $size="thumbnail", $attrs=array())
 {
     $value = get_post_meta($post_id, $key, true);
-    
+
     if (!empty($value)) {
         $value = unserialize($value[0]);
     } else {
-        return array();   
+        return array();
     }
-    
+
     $attachments = array();
     foreach($value as $id)
     {
@@ -72,6 +75,6 @@ function get_post_media($post_id, $key, $size="thumbnail", $attrs=array())
             );
         }
     }
-    
+
     return $attachments;
 }

12c72e7757c042372473716ce82268a9d0097d2e
Paul Houser <paul@plaidscreen.com>
2016-03-03 12:06:23 -0500

    Metabox bug fixes

--- class.MetaBox.php
+++ class.MetaBox.php
@@ -9,36 +9,36 @@ namespace OUW\MetaBox;
 
 class MetaBox {
 
-    private $fields, $post_types, $perms;    
+    private $fields, $post_types, $perms;
     public $id, $title, $atts;
-    
+
     const FIELD_TEMPLATE  = '<p class="field">%s</p>';
     const FIELD_TEMPLATE_LABEL = '<label for="%1$s">%3$s</label>';
     const FIELD_TEMPLATE_INPUT = '<input id="%1$s" name="%2$s" value="%4$s" %5$s>';
-        
+
     public function __construct($id, $title, $atts = array()) {
-        
+
         $abort = apply_filters("MetaBox/disable_metabox/$id", false);
         if ($abort)
             return false;
-        
+
         $this->fields = array();
-        
+
         $post_types = isset($atts["post_types"]) ? $atts["post_types"] : "post";
-        
+
         if (is_string($post_types))
             $this->post_types = array($post_types);
         else if (is_array($post_types))
             $this->post_types = $post_types;
         else
             throw new Exception('Fatal error: optional attribute `$post_types` must be string or array');
-        
+
         $this->id = $id;
         $this->title = $title;
         $this->atts = $atts;
-        
+
         $this->perms = array();
-        foreach($this->post_types as $post_type) 
+        foreach($this->post_types as $post_type)
         {
             if (isset($atts["perms"]) && is_string($atts["perms"]))
                 $this->perms[$post_type] = $atts["perms"];
@@ -47,16 +47,16 @@ class MetaBox {
             else
                 $this->perms[$post_type] = "edit_{$post_type}";
         }
-        
+
         add_action('add_meta_boxes', array($this, 'build'));
         add_action('save_post', array($this, 'save'));
-        
+
         add_action('edit_form_after_title', array($this, 'deck_meta'));
-        
+
     }
-    
+
     public function add_field($name, $label, $attrs = array()) {
-                    
+
         $newfield = array(
             "label" => $label,
             "name" => $name,
@@ -64,40 +64,40 @@ class MetaBox {
             "attrs" => $attrs,
             "value" => ""
         );
-        
-        if (isset($newfield["attrs"]["type"])) 
+
+        if (isset($newfield["attrs"]["type"]))
         {
             $newfield["type"] = $newfield["attrs"]["type"];
             unset($newfield["attrs"]["type"]);
         }
-        
+
         if (isset($newfield["attrs"]["value"]))
         {
             $newfield["value"] = $newfield["attrs"]["value"];
-            unset($newfield["attrs"]["value"]);            
+            unset($newfield["attrs"]["value"]);
         }
-        
+
         if (!isset($newfield["attrs"]["class"]))
         {
             $newfield["attrs"]["class"] = "widefat";
         }
-        
-        
+
+
         $newfield = \apply_filters("MetaBox/add_field/{$newfield["type"]}", $newfield);
         $newfield = \apply_filters("MetaBox/add_field/$name", $newfield);
-        
+
         $this->fields[$name] = $newfield;
-        
+
     }
-        
+
     public function build() {
-        
+
         \do_action("MetaBox/before_registration", $this);
-            
-        foreach ($this->post_types as $post_type) 
+
+        foreach ($this->post_types as $post_type)
         {
             add_meta_box(
-                $this->id, 
+                $this->id,
                 $this->title,
                 array($this, 'render'),
                 $post_type,
@@ -105,75 +105,75 @@ class MetaBox {
                 isset($this->atts["priority"]) ? $this->atts["priority"] : 'default'
             );
         }
-        
+
         \do_action("MetaBox/after_registration", $this);
-        
+
     }
-    
+
     public function render($post) {
-        
+
         $formoutput = "";
-        
+
         \do_action("MetaBox/before_render", $this, $post);
-        
+
         $meta = \get_post_meta($post->ID);
-        
-        foreach($this->fields as $name => $field) 
+
+        foreach($this->fields as $name => $field)
         {
             $field = \apply_filters("MetaBox/render_field/properties", $field, $post);
             $id = $this->field_name($field);
-            
+
             $value = isset($meta[$name]) ? $meta[$name] : $field["value"];
             $value = \apply_filters("MetaBox/render_field/{$field["type"]}/value", $value, $field, $post);
-            
+
             $template = self::FIELD_TEMPLATE;
             $template_inside = self::FIELD_TEMPLATE_INPUT;
             if (!isset($field["attrs"]["label"]) || $field["attrs"]["label"] !== false)
                 $template_inside = self::FIELD_TEMPLATE_LABEL . $template_inside;
             $template = sprintf(self::FIELD_TEMPLATE, $template_inside);
             $template = \apply_filters("MetaBox/render_field/{$field["type"]}/template", $template, $field, $post, $value);
-            
-            
+
+
             if (is_array($value))
                 $value = current($value);
-            $output = sprintf($template, 
+            $output = sprintf($template,
                               $id,
-                              $id, 
+                              $id,
                               $field["label"],
                               $value,
                               self::split_to_input($field["attrs"]));
-          
+
             $output = apply_filters("MetaBox/render_field/output", $output, $field, $this, $post);
             $output = apply_filters("MetaBox/render_field/{$field["type"]}", $output, $field, $this, $post);
             $output = apply_filters("MetaBox/render_field/{$this->id}/$name", $output, $field, $this, $post);
-                                
+
             $formoutput .= $output;
-          
+
         }
-        
+
         $formoutput .= wp_nonce_field("MetaBox/save", "{$this->id}_nonce", true, false);
-        
+
         $formoutput = apply_filters("MetaBox/form_output/{$this->id}", $formoutput, $this, $post);
         $formoutput = apply_filters("MetaBox/form_output", $formoutput, $this, $post);
-        
+
         echo $formoutput;
-        
-        \do_action("MetaBox/after_render", $this, $post);        
-        
-        
+
+        \do_action("MetaBox/after_render", $this, $post);
+
+
     }
-    
+
     public function deck_meta() {
-             
+
         global $post, $wp_meta_boxes;
-        
+
         do_meta_boxes(get_current_screen(), 'deck', $post);
-        
+
         unset($wp_meta_boxes[get_post_type($post)]['deck']);
-        
+
     }
-    
-    static function split_to_input($attrs) 
+
+    static function split_to_input($attrs)
     {
         $output = "";
         foreach($attrs as $key => $value)
@@ -184,63 +184,63 @@ class MetaBox {
             else if ($value === true)
                 $output .= ' ' . $key . '="' . $key . '"';
         }
-      
+
         return $output;
     }
-    
-    private function field_name($field) 
+
+    private function field_name($field)
     {
         return apply_filters("MetaBox/field_name/{$this->id}/{$field["name"]}", $this->id . "-" . $field["name"], $field, $this);
     }
-    
-    public function save($post_id) 
+
+    public function save($post_id)
     {
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
             return;
-	   
+
         if (defined('DOING_AJAX') && DOING_AJAX)
             return;
-        
+
         if (!isset($_POST["{$this->id}_nonce"]))
             return;
-        
+
         if (!wp_verify_nonce($_POST["{$this->id}_nonce"], "MetaBox/save"))
             return;
-        
+
         $post = get_post($post_id);
         if (!$post)
             return;
-        
+
         if (!current_user_can($this->perms[$post->post_type], $post_id))
             return;
-        
+
         foreach($this->fields as $name => $field)
         {
-            $id = $this->field_name($field);            
-            
+            $id = $this->field_name($field);
+
             //if (!isset($_POST[$id]))
             //    continue;
-            
+
             $filter = apply_filters("MetaBox/field_filter/{$field["type"]}", FILTER_SANITIZE_STRING);
-                        
-            $value = $_POST[$id];
-            
+
+            $value = isset($_POST[$id]) ? $_POST[$id] : null;
+
             if ($filter!==false)
                 $value = filter_var($value, $filter);
-            
+
             $value = apply_filters("MetaBox/save_field", $value, $field, $this, $post_id);
             $value = apply_filters("MetaBox/save_field/{$field["type"]}", $value, $field, $this, $post_id);
             $value = apply_filters("MetaBox/save_field/{$this->id}/{$field["name"]}", $value, $field, $this, $post_id);
-            
+
             do_action("MetaBox/before_field_saved", $value, $field, $this, $post_id);
             do_action("MetaBox/before_field_saved/{$field["type"]}", $value, $field, $this, $post_id);
-            
+
             $meta_id = update_post_meta($post_id, $name, $value);
-            
+
             do_action("MetaBox/after_field_saved", $meta_id, $value, $field, $this, $post_id);
-            
+
         }
-        
+
     }
-                             
+
 }

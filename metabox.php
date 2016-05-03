8daefae4d64564b22925f00d44cbc2d32c2cf84e
Paul Houser <paul@plaidpowered.com>
2016-05-03 13:07:55 -0400

    Let's not use a non-existant function, okay\?

--- metabox.php
+++ metabox.php
@@ -1,11 +1,37 @@
 <?php
 
+namespace Metabox;
+
 include "class.MetaBox.php";
 
-OUW\autoload_folder(__DIR__."/fields");
+function autoload_folder($dir)
+{
+    $contained = scandir($dir);
 
+    foreach($contained as $file)
+    {
+        if (substr($file,0,1) === '.')
+            continue;
 
-add_action("admin_enqueue_scripts", function() 
+        if (is_dir($dir.'/'.$file) && is_file("$dir/$file/$file.php"))
+        {
+            include_once "$dir/$file/$file.php";
+            continue;
+        }
+
+        if (!is_file("$dir/$file"))
+            continue;
+
+        if (pathinfo("$dir/$file", PATHINFO_EXTENSION) != 'php')
+            continue;
+
+        include_once $dir . '/' . $file;
+    }
+}
+
+autoload_folder(__DIR__."/fields");
+
+add_action("admin_enqueue_scripts", function()
 {
     wp_enqueue_style("ouw-metaboxes", OUW\theme_url(__DIR__."/style.css"));
     wp_register_script('metabox-term-icons', OUW\theme_url(__DIR__."/terms/icons.js"), array("jquery"), null, true);
